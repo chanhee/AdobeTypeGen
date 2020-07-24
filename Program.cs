334aaaf7a40676d96d2668d79388808ef90698a6
chanhee <jch0220@gmail.com>
2020-07-24 16:20:32 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+
+namespace AdobeTypeGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) {
+                Console.Error.WriteLine("Usage: AdobeTypeGen <adobe-type>.xml");
+                return;
+            }
+
+            var xmlPath = args[0];
+
+            if (!File.Exists(xmlPath)) {
+
+            }
+            var xmlDoc = new XmlDocument();
+            // xmlDoc.load
+        }
+    }
+}
