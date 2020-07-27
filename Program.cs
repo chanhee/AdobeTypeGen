32f670ac7032842f98f2b37963a0c25d3a456bac
chanhee <jch0220@gmail.com>
2020-07-27 17:54:57 +0900

    Linq to xml

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Xml;
+using System.Xml.Linq;
 
 
 namespace AdobeTypeGen
@@ -20,30 +23,89 @@ namespace AdobeTypeGen
                 Console.Error.WriteLine($"File not exists: {xmlPath}");
             }
 
-            var xmlDoc = new XmlDocument();
-            xmlDoc.Load(xmlPath);
+            // dictionary
+            // - map
+            //  - topicref
+            //   - topicref
+            // - package
+            //  - classdef
+            //   - elements
+            //    - property
+            //    - method
+            var xmlDoc = XElement.Load(xmlPath);
+            var classDefs = GenerateClassDefs(xmlDoc);
+        }
 
-            var classes = xmlDoc.GetElementsByTagName("classdef");
-            foreach (XmlNode cc in classes) {
-                Console.WriteLine(cc.Attributes["name"].Value);
+        public static List<ClassDef> GenerateClassDefs(XElement xmlDoc)
+        {
+            var ns = xmlDoc.GetDefaultNamespace();
+            XName NsName(string name) => ns + name;
+
+            // INFO: <topicref navtitle="Collections">
+            var collections = xmlDoc.Descendants(NsName("topicref"))
+                .First(x => (string)x.Attribute("navtitle") == "Collections")
+                .Descendants().Select(x => (string)x.Attribute("navtitle"))
+                .ToList();
+
+            var classDefs = new List<ClassDef>();
+            var classes = xmlDoc.Descendants(NsName("classdef"));
+            foreach (var cls in classes) {
+                var classDef = new ClassDef();
+                classDef.Name = (string)cls.Attribute("name");
+                classDef.IsEnum = (string)cls.Attribute("enumeration") == "true";
+                classDef.IsCollection = collections.IndexOf(classDef.Name) > -1;
+                classDef.Description = (string)cls.Element(NsName("shortdesc"));
+                classDef.SuperClass = (string)cls.Element(NsName("superclass"));
+
+                Console.WriteLine(classDef + ": " + classDef.Description);
+                classDefs.Add(classDef);
             }
+
+            return classDefs;
         }
     }
 
     public class ClassDef
     {
         public string Name { get; set; }
+        public string SuperClass { get; set; }
         public bool IsEnum { get; set; }
         public string Description { get; set; }
+        public bool IsCollection { get; set; }
+        public override string ToString()
+        {
+            if (IsEnum) {
+                return $"{Name}: Enum";
+            }
+            if (IsCollection) {
+                return $"{Name}: Collection";
+            }
+            return $"{Name}: Class";
+        }
     }
 
     public class Property
     {
-
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public bool IsArray { get; set; }
+        public object Value { get; set; }
+        public string Description { get; set; }
+        public bool ReadOnly { get; set; }
+        public bool IsStatic { get; set; }
     }
 
     public class Method
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ReturnType { get; set; }
+    }
 
+    public class Parameter
+    {
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public string Description { get; set; }
     }
 }
